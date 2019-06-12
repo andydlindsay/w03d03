d3f99e37115ff65235ef08e4f4b17d93b7847ffa
Andy Lindsay <andydlindsay@gmail.com>
2019-06-12 10:14:00 -0700

    added stylesheet and refactored app.js

--- public/app.js
+++ public/app.js
@@ -1,9 +1,19 @@
+function createPost(post) {
+    const $article = $('<article>');
+    const $title = $('<header>').text(`Title: ${post.title}`);
+    const $body = $(`<p>${post.body}</p>`);
+    const $footer = $('<footer>').text(`Author: ${post.userId}`);
+    $article.append($title, $body, $footer).appendTo('#posts');
+}
+
 $(() => {
     const BASE_URL = 'https://jsonplaceholder.typicode.com';
 
     $('#fetch-posts').click(() => {
         $.getJSON(`${BASE_URL}/posts`, (data) => {
-            console.log(data);
+            const $posts = $('#posts');
+            $posts.empty();
+            data.slice(0, 10).forEach(createPost);
         });
     });
       
