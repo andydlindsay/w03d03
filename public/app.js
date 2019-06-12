8e795b776587bfd5e66c1b4e402d70b1178a6a45
Andy Lindsay <andydlindsay@gmail.com>
2019-06-12 10:42:09 -0700

    moved createPost function inside document.ready and added html form for new posts

--- public/app.js
+++ public/app.js
@@ -1,19 +1,28 @@
-function createPost(post) {
-    const $article = $('<article>');
-    const $title = $('<header>').text(`Title: ${post.title}`);
-    const $body = $(`<p>${post.body}</p>`);
-    const $footer = $('<footer>').text(`Author: ${post.userId}`);
-    $article.append($title, $body, $footer).appendTo('#posts');
-}
-
 $(() => {
     const BASE_URL = 'https://jsonplaceholder.typicode.com';
+
+    function createPost(post) {
+        const $article = $('<article>');
+        const $title = $('<header>').text(`Title: ${post.title} (${post.id})`);
+        const $body = $(`<p>${post.body}</p>`);
+        $.getJSON(`${BASE_URL}/users/${post.userId}`, (user) => {
+            const $footer = $('<footer>').text(`Author: ${user.name} (${user.email})`);
+            $article.append($title, $body, $footer).appendTo('#posts');
+        });
+    }
 
     $('#fetch-posts').click(() => {
         $.getJSON(`${BASE_URL}/posts`, (data) => {
             const $posts = $('#posts');
             $posts.empty();
-            data.slice(0, 10).forEach(createPost);
+            data.slice(5, 15).forEach(createPost);
+        });
+    });
+
+    $('#new-post').on('submit', (event) => {
+        event.preventDefault();
+        $.post(`${BASE_URL}/posts`, $('#new-post').serialize(), (newPost) => {
+            createPost(newPost);
         });
     });
       
